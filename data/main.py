1bea74da7e47905ef82dbb9050cfa7182f6b2f4c
Rokas Puzonas <rokas.puz@gmail.com>
2023-12-27 17:19:00 +0200

    update data cleaning code

--- data/main.py
+++ data/main.py
@@ -1,5 +1,5 @@
-import os
-
+import os.path as path
+import zipfile
 
 def ConcatColumn(originalfilePath, columnFilePath, outputFilePath, delimiter=' '):
     with open(originalfilePath, 'r') as originalFile, open(columnFilePath, 'r') as columnFile, open(
@@ -7,7 +7,6 @@ def ConcatColumn(originalfilePath, columnFilePath, outputFilePath, delimiter=' '
         for originalLine, columnLine in zip(originalFile, columnFile):
             concatenatedLine = f"{originalLine.strip()}{delimiter}{columnLine.strip()}\n"
             outputFile.write(concatenatedLine)
-
 
 def CalculateAverageLength(filePath):
     totalLength = 0
@@ -19,13 +18,11 @@ def CalculateAverageLength(filePath):
     averageLength = totalLength / lineCount if lineCount > 0 else 0
     return averageLength
 
-
 def FilterAndWrite(inputFilePath, averageLength, outputFilePath):
     with open(inputFilePath, 'r') as inputFile, open(outputFilePath, 'w') as outputFile:
         for line in inputFile:
             if len(line) <= 2 * averageLength:
                 outputFile.write(line)
-
 
 def CountCommands(line, commandMapping):
     counts = {key: 0 for key in commandMapping}
@@ -35,7 +32,6 @@ def CountCommands(line, commandMapping):
         if command in counts:
             counts[command] += 1
     return counts
-
 
 def ProcessInputFile(inputFilePath, outputFilePath, commandMapping):
     with open(inputFilePath, 'r') as inputFile, open(outputFilePath, 'w') as outputFile:
@@ -50,7 +46,6 @@ def ProcessInputFile(inputFilePath, outputFilePath, commandMapping):
                 lastWord = line.strip().split()[-1]
                 outputFile.write(f"{lineNum},{countsStr},{lastWord}\n")
 
-
 def ReadMappingFile(filePath):
     commandMapping = {}
     with open(filePath, 'r') as file:
@@ -63,20 +58,92 @@ def ReadMappingFile(filePath):
                     commandMapping[key.strip()] = int(value.strip())
     return commandMapping
 
+def readApiIndex(filename: str) -> list[str]:
+    funciton_names = []
+    with open(filename, "r") as f:
+        for line in f:
+            if len(line) == 0: continue
+            parts = line.split("=", 1)
+            if len(parts) != 2: continue
+            name = parts[0]
+            if name.startswith("__"): continue
+            funciton_names.append(name)
+    return funciton_names
+
+def countApiCallOccurences(filename: str, api_index: list[str]) -> list[dict[str, int]]:
+    occurences = []
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            counts = {}
+            for api_call in line.split(" "):
+                if api_call not in counts:
+                    counts[api_call] = 0
+                counts[api_call] += 1
+            occurences.append(counts)
+    return occurences
+
+def readLabels(filename: str) -> list[str]:
+    labels = []
+    with open(filename, "r") as f:
+        for line in f:
+            labels.append(line.strip())
+    return labels
+
+def isApiCallUsed(api_counts: list[dict[str, int]], api_name: str) -> bool:
+    for counts in api_counts:
+        if counts.get(api_name, 0) > 0:
+            return True
+    return False
+
+def isApiCallCommon(api_counts: list[dict[str, int]], api_name: str) -> bool:
+    for counts in api_counts:
+        if counts.get(api_name, 0) == 0:
+            return False
+    return True
+
+def writeApiCounts(filename: str, api_counts: list[dict[str, int]], api_index: list[str]):
+    with open(filename, "w") as f:
+        f.write(",".join(api_index))
+        f.write(",target\n")
+        for i, row in enumerate(api_counts):
+            for api_call_name in api_index:
+                f.write(str(row.get(api_call_name, 0)))
+                f.write(",")
+            f.write(labels[i])
+            f.write("\n")
 
 if __name__ == "__main__":
-    original = 'all_analysis_data.txt'
-    columns = 'labels.csv'
-    output1 = 'output.csv'
-    output2 = 'output2.csv'
-    ConcatColumn(original, columns, output1)
-    averageLength = CalculateAverageLength(output1)
+    initialFolder = "initial"
 
-    FilterAndWrite(output1, averageLength, output2)
-    mappingFilePath = 'ApiIndex.txt'
-    commandMapping = ReadMappingFile(mappingFilePath)
-    finalOutput = 'data.csv'
-    ProcessInputFile(output2, finalOutput, commandMapping)
-    os.remove("output.csv")
-    os.remove("output2.csv")
-    print(f"Done")
+    original_path = initialFolder + "/all_analysis_data.txt"
+    if not path.exists(original_path):
+        with zipfile.ZipFile(initialFolder + "/mal-api-2019.zip") as data_zip:
+            data_zip.extract("all_analysis_data.txt", initialFolder)
+
+    print("Counting API call occurences...")
+    api_index  = readApiIndex(initialFolder + "/ApiIndex.txt")
+    labels     = readLabels(initialFolder + "/labels.csv")
+    api_counts = countApiCallOccurences(original_path, api_index)
+
+    print("Writing results to file...")
+    writeApiCounts("counted.csv", api_counts, api_index)
+
+    print("Writing results to file (skip columns with no calls)...")
+    trimmed_api_index = []
+    for call_name in api_index:
+        if not isApiCallUsed(api_counts, call_name): continue
+        trimmed_api_index.append(call_name)
+
+    writeApiCounts("counted-trimmed.csv", api_counts, trimmed_api_index)
+
+    print("Writing results to file (skip common columns)...")
+    unique_api_index = []
+    for call_name in trimmed_api_index:
+        if isApiCallCommon(api_counts, call_name): continue
+        unique_api_index.append(call_name)
+
+    writeApiCounts("counted-unique.csv", api_counts, unique_api_index)
+
+    print(f"Done!")
+
